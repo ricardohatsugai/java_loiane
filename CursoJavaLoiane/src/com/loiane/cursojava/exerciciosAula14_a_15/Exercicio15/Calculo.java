1673b554ac8e9d78ae0421e5e58a3f98738d83bb
Ricardo Hatsugai <rhatsugai@gmail.com>
2025-01-08 20:41:51 -0300

    Exercício 5 da lista 15

--- CursoJavaLoiane/src/com/loiane/cursojava/exerciciosAula14_a_15/Exercicio15/Calculo.java
+++ CursoJavaLoiane/src/com/loiane/cursojava/exerciciosAula14_a_15/Exercicio15/Calculo.java
@@ -1,4 +1,12 @@
+/* Projeto..: Calculo.java;
+ * Autor....: Ricardo Hatsugai
+ * Data.....: 07/01/2025
+ * Versão...: 1.2
+ * Descrição: Classe de regras de negócio à ser usado por todo sistema. */
+
 package com.loiane.cursojava.exerciciosAula14_a_15.Exercicio15;
+
+import javax.swing.JOptionPane;
 
 public class Calculo {
 	private double n1;
@@ -10,6 +18,27 @@ public class Calculo {
 		setN1(n1);
 		setN2(n2);
 		setMedia((n1+n2)/2);
+	}
+	
+	// método para classificar aprovação
+	public void aprovacao() {
+		String message;
+		
+		if(this.getMedia() >= 7.0 && this.getMedia() < 10.0) {
+			message = String.format("A média do aluno é %.2f, "+
+					"portanto ele está aprovado", this.getMedia());
+			JOptionPane.showMessageDialog(null, message);
+			
+		}else if(this.getMedia() < 7.0) {
+				message = String.format("A média do aluno é %.2f, "+
+						"portanto ele está Reprovado", this.getMedia());
+				JOptionPane.showMessageDialog(null, message);
+				
+		}else if (this.getMedia() == 10.0) {
+				message = String.format("A média do aluno é %.2f, portanto"+
+						" ele está Aprovado com Distinção.", this.getMedia());
+				JOptionPane.showMessageDialog(null, message);
+		}
 	}
 	
 	
--- CursoJavaLoiane/src/com/loiane/cursojava/exerciciosAula14_a_15/Exercicio15/Exercicio05.java
+++ CursoJavaLoiane/src/com/loiane/cursojava/exerciciosAula14_a_15/Exercicio15/Exercicio05.java
@@ -1,3 +1,10 @@
+/* Projeto..: Exercicio05.java;
+ * Autor....: Ricardo Hatsugai
+ * Data.....: 07/01/2025
+ * Versão...: 1.2
+ * Descrição: Uma classe entre as demais que poderão usar as regras de negócios 
+ * instanciadas como objeto. */
+
 package com.loiane.cursojava.exerciciosAula14_a_15.Exercicio15;
 
 import javax.swing.JOptionPane;
@@ -7,12 +14,16 @@ public class Exercicio05 {
 	public static void main(String[] args) {
 		double n1 = Double.parseDouble(JOptionPane.showInputDialog("Digite a nota N1: "));
 		double n2 = Double.parseDouble(JOptionPane.showInputDialog("Digite a nota N2: "));
+		String message;
 		
-		Calculo calculo1 = new Calculo(5.8, 7.3);
+		// Instancia classe com método construtor.
+		Calculo calculo1 = new Calculo(n1, n2); 
 		
-		String message = String.format("A média das notas n1 = %.2f e n2 = %.2f é %.2f\n", 
+		message = String.format("A média das notas n1 = %.2f e n2 = %.2f é %.2f\n", 
 				calculo1.getN1(), calculo1.getN2(), calculo1.getMedia());
 		JOptionPane.showMessageDialog(null, message);
+		
+		calculo1.aprovacao(); // Calcula se aprovado, Reprovado ou Aprovado com Distinção.
 		
 	}
 
